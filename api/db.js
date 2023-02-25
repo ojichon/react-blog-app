62a0d2b769c8c0fa78e10790818c13122f7dd924
ojichon <misohero@gmail.com>
2023-02-25 17:32:31 -0500

    lol

---
+++ api/db.js
@@ -0,0 +1,11 @@
+import mysql from "mysql"
+
+
+export const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Jxe61pq8910",
+    database: "blog"
+
+    
+})
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        A Chonny Ji  <b>Company</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
